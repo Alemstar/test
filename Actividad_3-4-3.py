f4bcb7ebca788465c806afd317723f0881849e54
Alemstar <al.faguas@duocuc.cl>
2024-06-12 22:26:24 -0400

    ejercicio 3-4-3 finalizado

--- Actividad_3-4-3.py
+++ Actividad_3-4-3.py
@@ -1,9 +1,31 @@
+pares = []
+impares = []
+def valida_numeros (list):
+        for num in list:
+            try:
+                int(num)
+            except:
+                 print ("Ingresó valor no valido")
+                 return "No valido"
+            return ("Valido")
 
-num = input("Ingrese una lista de núymeros separados por espacios ")
-lista_de_num = (num.split())
+validador = "No valido"
+while validador == "No valido":
+     nums = input("Ingresa una lista de números separados por espacio ")
+     nums_list = nums.split()
 
-print(lista_de_num)
-try:
-    x = [int(lista_de_num)]
-except ValueError:
-    print ("Debe ingresar un valor númerico")
+     validador = valida_numeros(nums_list)
+     print ("moshi")
+
+def par_o_impar(nums_list):
+    for num in nums_list:
+        numero=int(num)
+        numero = numero%2
+        if numero == 0:
+            pares.append(num)
+        else:
+            impares.append(num)
+
+par_o_impar(nums_list)
+print ("Los números pares son: ", pares)
+print ("Los números impares son: ", impares)
